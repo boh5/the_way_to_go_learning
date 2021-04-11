7eda8e2e5258a15201f2ec85374df49411d32283
dilless <bohuang1994@gmail.com>
2021-04-11 19:55:11 +0800

    c_6_8

---
+++ chapter_6/c_6_8/practice_6_8.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fv := func() {
+		fmt.Println("Hello World")
+	}
+	fv()
+}
---
+++ chapter_6/c_6_8/return_defer.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(f())
+}
+
+func f() (ret int) {
+	defer func() {
+		ret++
+	}()
+	return 1
+}
