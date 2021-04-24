b24c1a64e2057dcb79a96cf1b427d421ae786b5c
dilless <bohuang1994@gmail.com>
2021-04-24 14:47:52 +0800

    c_9_5

---
+++ chapter_9/c_9_5/pack1/pack1.go
@@ -0,0 +1,10 @@
+package pack1
+
+var (
+	Pack1Int   = 42
+	pack1Float = 3.14
+)
+
+func ReturnStr() string {
+	return "Hello main!"
+}
---
+++ chapter_9/c_9_5/package_mytest.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"the_way_to_go_learning/chapter_9/c_9_5/pack1"
+)
+
+func main() {
+	var test1 string
+	test1 = pack1.ReturnStr()
+	fmt.Printf("ReturnStr from package1: %s\n", test1)
+	fmt.Printf("Integer from package1: %d\n", pack1.Pack1Int)
+}
