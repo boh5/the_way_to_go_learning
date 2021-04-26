05333ddd21f6ff2c8e4ad82c6916a26d87769f73
dilless <bohuang1994@gmail.com>
2021-04-26 21:17:21 +0800

    c_11_10

---
+++ chapter_11/c_11_10/reflect2.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	var x float64 = 3.4
+	v := reflect.ValueOf(x)
+	// setting a value:
+	//v.SetFloat(3.1415) // Error: will panic: reflect.Value.SetFloat using unaddressable value
+	fmt.Println("settability of v:", v.CanSet())
+	v = reflect.ValueOf(&x)
+	fmt.Println("type of v:", v.Type())
+	fmt.Println("settability of v:", v.CanSet())
+	v = v.Elem()
+	fmt.Println("type of v:", v.Type())
+	fmt.Println("The Elem of v is:", v)
+	fmt.Println("settability of v:", v.CanSet())
+	v.SetFloat(3.1415)
+	fmt.Println(v.Interface())
+	fmt.Println(v)
+}
