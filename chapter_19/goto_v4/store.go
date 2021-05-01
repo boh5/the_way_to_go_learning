fa538751dd2620b86beb7b8168a76d2e92af9adc
dilless <bohuang1994@gmail.com>
2021-05-01 22:05:42 +0800

    chapter_19

---
+++ chapter_19/goto_v4/store.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"os"
+	"sync"
+)
+
+const saveQueueLength = 1000
+
+type URLStore struct {
+	urls map[string]string
+	mu   sync.RWMutex
+	save chan record
+}
+
+type record struct {
+	Key, URL string
+}
+
+func NewURLStore(filename string) *URLStore {
+	s := &URLStore{
+		urls: make(map[string]string),
+		save: make(chan record, saveQueueLength),
+	}
+	if err := s.load(filename); err != nil {
+		log.Println("Error loading data in URLStore:", err)
+	}
+	go s.saveLoop(filename)
+	return s
+}
+
+func (s *URLStore) Get(key string) string {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	return s.urls[key]
+}
+
+func (s *URLStore) Set(key, url string) bool {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if _, present := s.urls[key]; present {
+		return false
+	}
+	s.urls[key] = url
+	return true
+}
+
+func (s *URLStore) Count() int {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	return len(s.urls)
+}
+
+func (s *URLStore) Put(url string) string {
+	for {
+		key := genKey(s.Count())
+		if ok := s.Set(key, url); ok {
+			s.save <- record{key, url}
+			return key
+		}
+	}
+}
+
+func (s *URLStore) saveLoop(filename string) error {
+	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatal("URLStore:", err)
+	}
+	defer f.Close()
+	e := json.NewEncoder(f)
+	for {
+		r := <-s.save
+		if err := e.Encode(r); err != nil {
+			log.Println("URLStore:", err)
+		}
+	}
+}
+
+func (s *URLStore) load(filename string) error {
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Println("Error opening URLStore:", err)
+		return err
+	}
+	defer f.Close()
+	if _, err := f.Seek(0, 0); err != nil {
+		return err
+	}
+	d := json.NewDecoder(f)
+	for err == nil {
+		var r record
+		if err = d.Decode(&r); err == nil {
+			s.Set(r.Key, r.URL)
+		}
+	}
+	if err == io.EOF {
+		return nil
+	}
+	return err
+}
---
+++ chapter_19/goto_v5/store.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"log"
+	"net/rpc"
+	"os"
+	"sync"
+)
+
+const saveQueueLength = 1000
+
+type Store interface {
+	Put(url, key *string) error
+	Get(key, url *string) error
+}
+
+type URLStore struct {
+	urls map[string]string
+	mu   sync.RWMutex
+	save chan record
+}
+
+type ProxyStore struct {
+	urls   *URLStore
+	client *rpc.Client
+}
+
+type record struct {
+	Key, URL string
+}
+
+func NewURLStore(filename string) *URLStore {
+	s := &URLStore{urls: make(map[string]string)}
+	if filename != "" {
+		s.save = make(chan record, saveQueueLength)
+		if err := s.load(filename); err != nil {
+			log.Println("Error loading URLStore:", err)
+		}
+		go s.saveLoop(filename)
+	}
+	return s
+}
+
+func NewProxyStore(addr string) *ProxyStore {
+	client, err := rpc.DialHTTP("tcp", addr)
+	if err != nil {
+		log.Println("Error constructing ProxyStore:", err)
+	}
+	return &ProxyStore{urls: NewURLStore(""), client: client}
+}
+
+func (s *URLStore) Get(key, url *string) error {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	if u, ok := s.urls[*key]; ok {
+		*url = u
+		return nil
+	}
+	return errors.New("key not found")
+}
+
+func (s *URLStore) Set(key, url *string) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if _, present := s.urls[*key]; present {
+		return errors.New("key already exists")
+	}
+	s.urls[*key] = *url
+	return nil
+}
+
+func (s *URLStore) Count() int {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	return len(s.urls)
+}
+
+func (s *URLStore) Put(url, key *string) error {
+	for {
+		*key = genKey(s.Count())
+		if err := s.Set(key, url); err == nil {
+			break
+		}
+	}
+	if s.save != nil {
+		s.save <- record{*key, *url}
+	}
+	return nil
+}
+
+func (s *URLStore) saveLoop(filename string) error {
+	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatal("URLStore:", err)
+	}
+	defer f.Close()
+	e := json.NewEncoder(f)
+	for {
+		r := <-s.save
+		if err := e.Encode(r); err != nil {
+			log.Println("URLStore:", err)
+		}
+	}
+}
+
+func (s *URLStore) load(filename string) error {
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Println("Error opening URLStore:", err)
+		return err
+	}
+	defer f.Close()
+	if _, err := f.Seek(0, 0); err != nil {
+		return err
+	}
+	d := json.NewDecoder(f)
+	for err == nil {
+		var r record
+		if err = d.Decode(&r); err == nil {
+			s.Set(&r.Key, &r.URL)
+		}
+	}
+	if err == io.EOF {
+		return nil
+	}
+	return err
+}
+
+func (s *ProxyStore) Get(key, url *string) error {
+	if err := s.urls.Get(key, url); err == nil {
+		return nil
+	}
+	if err := s.client.Call("Store.Get", key, url); err != nil {
+		return err
+	}
+	s.urls.Set(key, url)
+	return nil
+}
+
+func (s *ProxyStore) Put(url, key *string) error {
+	if err := s.client.Call("Store.Put", url, key); err != nil {
+		return err
+	}
+	s.urls.Set(key, url)
+	return nil
+}
